0fc423ecf6bc61a5919cc5f1de0e999b5dad7eb3
bitcse6c <bitcsec3@gmail.com>
2025-06-10 11:00:35 +0530

    inicial

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Name: Abhishek Srivatsav" );
+        System.out.println( "USN: 1BI23CS400" );
+    }
+}
